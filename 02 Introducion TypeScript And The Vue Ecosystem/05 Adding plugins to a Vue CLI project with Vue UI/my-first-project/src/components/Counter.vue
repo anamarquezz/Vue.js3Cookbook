c5fb65f9f6fa5c1b41536f43518b698340bd66d7
anamarquez <anamarquez.edu@hotmail.com>
2021-06-09 16:45:44 -0700

    Creating your first TypeScript Vue component with vue-class-component

---
+++ 02 Introducion TypeScript And The Vue Ecosystem/05 Adding plugins to a Vue CLI project with Vue UI/my-first-project/src/components/Counter.vue
@@ -0,0 +1,23 @@
+
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component
+export default class Counter extends Vue {
+    valueNumber: number = 0;
+    get formattedNumber() {
+        return `Your total number is: ${this.valueNumber}`;
+    }
+
+    increase() {
+        this.valueNumber += 1;        
+    }
+
+    decrease() {
+        this.valueNumber -= 1;
+    }
+}
+</script>
