8947b0c2584a957aa9f111e77f0f10fb5e9057e6
anamarquez <anamarquez.edu@hotmail.com>
2021-06-25 17:18:30 -0700

    lastchanges

---
+++ 02 Introducion TypeScript And The Vue Ecosystem/08 Creating a custom function decorator with vue-class-component/my-first-project/src/components/Counter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <fieldset>
+      <legend>{{ formattedNumber }}</legend>
+      <button @click="increase">
+        Increase
+      </button><button @click="decrease">
+        Decrease
+      </button>
+    </fieldset>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component, { mixins } from 'vue-class-component';
+import DefaultNumber from '../mixins/defaultNumber';
+
+@Component
+export default class Counter extends mixins(DefaultNumber) {
+  valueNumber: number = 0;
+
+  increase() {
+    this.valueNumber += 1;
+  }
+
+  decrease() {
+    this.valueNumber -= 1;
+  }
+}
+</script>
